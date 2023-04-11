8a4de888377650943ef560eb5a0957180c25ea65
pshho <awdr498@gmail.com>
2023-04-11 18:02:20 +0900

    new v2

--- sql/dept.sql
+++ sql/dept.sql
@@ -30,7 +30,19 @@ COMMIT;
 SELECT * FROM dept;
 SELECT deptno as 부서코드, deptname as 부서명 FROM dept;
 
+-- 특정한 자료검색: WHERE 절 조건
 SELECT * FROM dept WHERE deptno = '1002';
+
+-- 자료 정렬: ORDER BY 칼럼명(ASC/DESC)
+SELECT * FROM dept ORDER BY deptno ASC /* DESC */;
+
+-- 부서번호 1002인 부서를 '회계팀'으로 변경하시오
+UPDATE dept
+SET deptname = '회계팀'
+WHERE deptno = 1002;
 
 /* 부서 테이블 삭제 */
 DROP TABLE dept;
+
+DELETE FROM dept
+WHERE deptno = '1003';
